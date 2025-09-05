bfc325de0cbd96266382448f6f4bbe333ea82119
Dantte2 <dino.sassi1@hotmail.fi>
2025-09-05 14:39:07 +0300

    weird animation bug

--- main.js
+++ main.js
@@ -39,18 +39,19 @@ function preload() {
         frameWidth: 64,
         frameHeight: 64
     });
+
+    this.load.spritesheet('playerattack', 'assets/player/attack/attack-01-sheet.png', {
+        frameWidth: 96,
+        frameHeight: 64
+    });
 }
 
 function create() {
-    // Create player sprite from idle spritesheet
     this.player = this.physics.add.sprite(400, 300, 'playerIdle', 0);
     this.player.setOrigin(0.5, 1);
     this.player.setCollideWorldBounds(true);
+    this.player.body.setSize(48, 64);
 
-    // Set physics body size to better match idle sprite
-    this.player.body.setSize(48, 64); // narrower than sprite width for better collision
-
-    // idle animation
     this.anims.create({
         key: 'idle',
         frames: this.anims.generateFrameNumbers('playerIdle', { start: 0, end: 3 }),
@@ -58,7 +59,6 @@ function create() {
         repeat: -1
     });
 
-    // run animation
     this.anims.create({
         key: 'walk',
         frames: this.anims.generateFrameNumbers('playerwalk', { start: 0, end: 7 }),
@@ -66,7 +66,6 @@ function create() {
         repeat: -1
     });
 
-    // jump animation
     this.anims.create({
         key: 'jump',
         frames: this.anims.generateFrameNumbers('playerjump', { start: 0, end: 3 }),
@@ -74,47 +73,79 @@ function create() {
         repeat: 0
     });
 
-    // landing animation
     this.anims.create({
         key: 'land',
         frames: this.anims.generateFrameNumbers('playerland', { start: 0, end: 2 }),
-        frameRate: 8,
+        frameRate: 10,
         repeat: 0
     });
 
-    // Play idle animation by default
+    this.anims.create({
+        key: 'attack',
+        frames: this.anims.generateFrameNumbers('playerattack', { start: 0, end: 7 }),
+        frameRate: 10,
+        repeat: 0
+    });
+
     this.player.play('idle');
 
-    // Create ground
     this.ground = this.physics.add.staticGroup();
     this.ground.create(400, 590, null).setDisplaySize(800, 20).refreshBody();
-
-    // Collisions
     this.physics.add.collider(this.player, this.ground);
 
-    // Player movement config
     this.playerSpeed = 200;
     this.jumpVelocity = -600;
-
-    // Add some horizontal drag for smoother stops
     this.player.body.setDragX(800);
 
-    // Input keys
     this.cursors = this.input.keyboard.createCursorKeys();
     this.wasd = this.input.keyboard.addKeys({
-        up: Phaser.Input.Keyboard.KeyCodes.W,
+        space: Phaser.Input.Keyboard.KeyCodes.SPACE,
         left: Phaser.Input.Keyboard.KeyCodes.A,
         down: Phaser.Input.Keyboard.KeyCodes.S,
         right: Phaser.Input.Keyboard.KeyCodes.D
     });
 
-    // Variables to track vertical velocity and landing state
+    this.isAttacking = false;
+
+    this.input.on('pointerdown', (pointer) => {
+        if (
+            pointer.leftButtonDown() &&
+            !this.isAttacking &&
+            this.player.body.blocked.down // only allow attack on ground
+        ) {
+            this.isAttacking = true;
+            this.player.play('attack');
+        }
+    });
+
+    this.player.on('animationstart-attack', () => {
+        this.isAttacking = true;
+    });
+
+    this.player.on('animationcomplete-attack', () => {
+        this.isAttacking = false;
+
+        const body = this.player.body;
+        const onGround = body.blocked.down;
+
+        const isMoving = this.cursors.left.isDown || this.cursors.right.isDown || this.wasd.left.isDown || this.wasd.right.isDown;
+
+        if (onGround) {
+            if (isMoving) {
+                this.player.play('walk');
+            } else {
+                this.player.play('idle');
+            }
+        } else {
+            this.player.play('jump');
+        }
+    });
+
     this.prevVelocityY = 0;
     this.falling = false;
     this.landingPlaying = false;
     this.wasOnGround = true;
 
-    // When landing animation completes, go to idle if still landing
     this.player.on('animationcomplete-land', () => {
         if (this.landingPlaying) {
             this.player.play('idle');
@@ -125,68 +156,68 @@ function create() {
 
 function update() {
     const body = this.player.body;
+    const onGround = body.blocked.down;
     let velocityX = 0;
 
-    // Left/right movement
-    if (this.cursors.left.isDown || this.wasd.left.isDown) {
-        velocityX = -this.playerSpeed;
-        this.player.setFlipX(true);
-        this.player.body.setSize(60, 64);
-    } else if (this.cursors.right.isDown || this.wasd.right.isDown) {
-        velocityX = this.playerSpeed;
-        this.player.setFlipX(false);
-        this.player.body.setSize(60, 64);
+    const justLanded = !this.wasOnGround && onGround;
+
+    const animKey = this.player.anims.currentAnim?.key;
+
+    // Block movement/jump input if attacking
+    const blockAnim = (animKey === 'attack');
+
+    if (blockAnim) {
+        body.setVelocityX(0);
     } else {
-        this.player.body.setSize(48, 64);
-    }
-
-    body.setVelocityX(velocityX);
-
-    const onGround = body.blocked.down;
-
-    // Detect start of fall (velocityY switches from <=0 to >0)
-    if (!onGround && this.prevVelocityY <= 0 && body.velocity.y > 0) {
-        this.falling = true;
-        this.player.play('land', true);  // Play landing animation when starting to fall
-    }
-
-    // Jump input
-    if ((this.cursors.up.isDown || this.wasd.up.isDown) && onGround) {
-        body.setVelocityY(this.jumpVelocity);
-        this.player.play('jump', true);
-        this.falling = false;
-        this.landingPlaying = false;
-    }
-
-    if (onGround) {
-        if (!this.wasOnGround) {
-            // Just landed on ground
-            this.landingPlaying = true;
-            this.falling = false;
-            this.player.play('land');
-        }
-
-        if (this.landingPlaying) {
-            if (velocityX !== 0) {
-                this.player.play('walk');
-                this.landingPlaying = false;
-            }
-            // else wait for landing animation to finish
+        if (this.cursors.left.isDown || this.wasd.left.isDown) {
+            velocityX = -this.playerSpeed;
+            this.player.setFlipX(true);
+            this.player.body.setSize(60, 64);
+        } else if (this.cursors.right.isDown || this.wasd.right.isDown) {
+            velocityX = this.playerSpeed;
+            this.player.setFlipX(false);
+            this.player.body.setSize(60, 64);
         } else {
-            if (velocityX !== 0) {
-                if (this.player.anims.currentAnim?.key !== 'walk') {
-                    this.player.play('walk');
-                }
+            this.player.body.setSize(48, 64);
+        }
+
+        body.setVelocityX(velocityX);
+
+        if ((this.cursors.space.isDown || this.wasd.space.isDown) && onGround) {
+            body.setVelocityY(this.jumpVelocity);
+            this.player.play('jump', true);
+            this.falling = false;
+            this.landingPlaying = false;
+        }
+    }
+
+    if (justLanded && !this.landingPlaying && !blockAnim) {
+        this.landingPlaying = true;
+        this.player.play('land');
+        // Removed the lastLandingTime and cooldown here
+    }
+
+    if (!blockAnim) {
+        if (!onGround) {
+            if (!this.falling && animKey !== 'jump') {
+                this.player.play('jump');
+            }
+            this.falling = true;
+        } else {
+            this.falling = false;
+            if (this.landingPlaying) {
+                // waiting for landing animation to finish
             } else {
-                if (this.player.anims.currentAnim?.key !== 'idle') {
-                    this.player.play('idle');
+                if (velocityX !== 0) {
+                    if (animKey !== 'walk') {
+                        this.player.play('walk');
+                    }
+                } else {
+                    if (animKey !== 'idle') {
+                        this.player.play('idle');
+                    }
                 }
             }
-        }
-    } else {
-        // Airborne and not falling start, keep jump animation if not already playing
-        if (!this.falling && this.player.anims.currentAnim?.key !== 'jump') {
-            this.player.play('jump');
         }
     }
 
